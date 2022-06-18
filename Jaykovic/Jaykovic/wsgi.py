6d598495c4ec5822b89c02738dc60a0601fec8af
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-06-18 23:10:40 +0100

    updated file

--- Jaykovic/Jaykovic/wsgi.py
+++ Jaykovic/Jaykovic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yusfate4 project.
+WSGI config for Jaykovic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yusfate4.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jaykovic.settings')
 
 application = get_wsgi_application()
